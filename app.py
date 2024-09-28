bc9cf0e65ae2965086ac220560cdea6c729e2bdf
Matt Mo <matthewlmo+git@gmail.com>
2024-09-28 02:35:53 -0400

    use chatgpt to parse fetched context

--- app.py
+++ app.py
@@ -24,7 +24,9 @@ gen_kwargs = {
 
 SYSTEM_PROMPT = """\
 You are a movie buff and you keep track of what the latest movies are.
-If a user asks for recent information, output a function call and the system add to the context.
+If a user asks for recent information, check if you already have the relevant context information. 
+If you do, then output the contextual information.
+If you do not have the context, then output a function call.
 If you need to call a function, only output the function call. 
 Call functions using Python syntax in plain text, no code blocks.
 
@@ -34,6 +36,17 @@ You have access to the following functions:
 - buy_ticket(theater, movie, showtime)
 - get_reviews(movie_id)
 """
+
+# SYSTEM_PROMPT_WITH_CONTEXT = """\
+# You are a movie buff and you keep track of what the latest movies are.
+# If your last response was with a function call such as the following:
+# - get_now_playing_movies()
+# - get_showtimes(title, location)
+# - buy_ticket(theater, movie, showtime)
+# - get_reviews(movie_id)
+
+# Review the message history for context in api_fetch_context. If you have the information, respond with it.
+# """
 
 @observe
 @cl.on_chat_start
@@ -52,12 +65,6 @@ async def generate_response(client, message_history, gen_kwargs):
             await response_message.stream_token(token)
     
     await response_message.update()
-    if "get_now_playing_movies" in response_message.content:
-        now_playing = get_now_playing_movies()
-        print(f"now_playing: {now_playing}")
-        response_message.content = now_playing
-        await response_message.update()
-
 
     return response_message
 
@@ -70,7 +77,15 @@ async def on_message(message: cl.Message):
     
     response_message = await generate_response(client, message_history, gen_kwargs)
 
-    message_history.append({"role": "assistant", "content": response_message.content})
+    # Check response for functions
+    if "get_now_playing_movies" in response_message.content:
+        now_playing = get_now_playing_movies()
+        message_history.append({"role": "system", "content": now_playing})
+        response_message = await generate_response(client, message_history, gen_kwargs)
+    else:
+        # Update message history as normal if there is no function in the latest response from the chatbot
+        message_history.append({"role": "assistant", "content": response_message.content})
+
     cl.user_session.set("message_history", message_history)
 
 if __name__ == "__main__":
